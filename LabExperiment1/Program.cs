b4dec4898e13d026ae33ec0c418fb02775c50df9
Ram1476 <ramilangovan.1299@gmail.com>
2023-02-01 19:23:56 +0530

    Add project files.

---
+++ LabExperiment1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabExperiment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Car car = new Car();
+            car.AcceptDetails();
+            car.DisplayDetails();
+            Console.ReadLine();
+        }
+    }
+}
